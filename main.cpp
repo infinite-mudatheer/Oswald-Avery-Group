3ba45faf90ba2019503f09e4d0347be0cf2651e3
adamumai <adamuusmanmaimsa@gmail.com>
2025-02-03 08:36:07 -0800

    Delete main.cpp
    Signed-off-by: adamumai <adamuusmanmaimsa@gmail.com>

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Name: Mudassir Hussaini" << std::endl;
-    std::cout << "Email: Mundansiruhussaini@gmail.com" << std::endl;
-    std::cout << "Phone number: 09043010480" << std::endl;
-    std::cout << "Area Of Interest: Genomics" << std::endl;
-    return 0;
-}
