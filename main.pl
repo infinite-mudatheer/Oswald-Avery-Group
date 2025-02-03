1a31f51fdbdc18f8a8412ceee2216ca914b9178c
Mudansiru Hussaini <mundansiruhussaini@gmail.com>
2025-02-03 04:23:36 -0800

    Update main.pl

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
 # Print details
-print "Name: AMEERA AMINU MIRAJ\n";
-print "Email: amiraaminu900@gmail.com\n";
+print "Name: AmiRA AMINU MIRAJ\n";
+print "Email: amiramiraj006@gmail.com\n";
 print "Phone number: 08067537006\n";
 print "Area Of Interest: DATA VISUALISATION\n";
